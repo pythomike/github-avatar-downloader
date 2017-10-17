6d883396af668c971bdaf7890be647add2f1e689
Mike <pythomike@gmail.com>
2017-10-17 16:46:40 +0000

    Added downloadImageByURL and support for command line arguments

--- download_avatars.js
+++ download_avatars.js
@@ -1,17 +1,13 @@
 var request = require('request');
 var fs = require('fs');
-
 var gitUser = "pythomike"
 var gitToken = "2ec27c2e1b2544efe5729adb36b3973844ad2d90"
 
-var repoOwner = "pythomike"  //  REMOVE
-var repoName = "waldo"      //   REMOVE
+var repoOwner = process.argv[2] 
+var repoName = process.argv[3]
 
-// var repoOwner = process.argv[2] 
-// var repoName = process.argv[3]
-
-// if (!repoOwner || !repoName) 
-//   console.log("You need to give me the repo owner, and the repo name. I'm missing one of those")
+if (!repoOwner || !repoName) 
+  console.log("You need to give me the repo owner, and the repo name. I'm missing one of those")
 
 function getRepoContributors(repoOwner, repoName, cb) {  
   var options = {
@@ -21,27 +17,24 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }
   }
   request(options, function(err, res, body) {
-      var outputBody = JSON.parse(body);
-      var contributorObj = []
-      for (each in outputBody){
-        contributorObj.push(outputBody[each])
-      }
-      cb(contributorObj)
+      var data = JSON.parse(body);
+      data.forEach(function(profile) {
+        if (!fs.existsSync("./avatars")) {
+          fs.mkdirSync("./avatars");
+        }
+        cb(profile.avatar_url, "avatars/" + profile.login + ".jpg");
     })
+})
 }
-
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-getRepoContributors(repoOwner, repoName, isolateAvatar)
+getRepoContributors(repoOwner, repoName, downloadImageByURL)
 
 function downloadImageByURL(url, filePath){ 
-}
-
-function isolateAvatar(contributors){
-  var userList = {}
-  for (each in contributors){
-    userList[contributors[each].login] = contributors[each].avatar_url
-  }
-  console.log(userList)
+  request.get(url)
+    .on('error', function(err) {
+      throw err;
+    })
+    .pipe(fs.createWriteStream(filePath))
 }
